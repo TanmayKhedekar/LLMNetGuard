0979cb5eb7eb5d96e0f7c928827ae0c7d0ed1839
Tanmay Khedekar <khedekartanmay50@gmail.com>
2025-01-04 22:46:44 +0530

    Update list.py

--- list.py
+++ list.py
@@ -3,7 +3,7 @@ import requests
 
 def monitor_packet(packet):
     info = str(packet.summary())
-    response = requests.post("http://127.0.0.1:8000/predict/", json={"text": info})
+    response = requests.post("http://127.0.0.9:2005/predict/", json={"text": info})
     print(f"Packet Info: {info} | Prediction: {response.json()}")
 
 sniff(prn=monitor_packet, count=10)  # Adjust count for real-time capture
